8915c26a3904bbc8142331c677015de55a3a9c92
taehyeong park <b_tae96@naver.com>
2022-05-27 17:15:19 +0900

    [update] redux 액션 함수 세팅

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,11 +1,12 @@
 //configStore.js
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import quiz from './modules/quiz'
+// import thunk from "redux-thunk";
+import memo from "./modules/memo";
+
 // root 리듀서를 만들어줍니다.
 // 나중에 리듀서를 여러개 만들게 되면 여기에 하나씩 추가해주는 거예요!
-const middlewares = [thunk];
-const rootReducer = combineReducers({ quiz });
+// const middlewares = [thunk];
+const rootReducer = combineReducers({ memo });
 // const enhancer = applyMiddleware(...middlewares);
 // 스토어를 만듭니다.
 const store = createStore(rootReducer); //, enhancer << store
---
+++ src/redux/modules/memo.js
@@ -0,0 +1,41 @@
+const initialState = [
+  {
+    id: "aaa",
+    word: "aaaaa",
+    comment: "이뜻입니다.",
+    ex: "이렇게 작성",
+    check: false,
+  },
+];
+
+// const LOAD = "memo/LOAD";
+// 액션
+const CREATE = "memo/CREATE";
+const UPDATE_check = "memo_check/UPDATE";
+const REMOVE = "memo/REMOVE";
+const DELETE = "memo/DETELE";
+
+//액션 함수
+export const createMemo = (memo) => {
+  return { type: CREATE, memo };
+};
+
+export const updateCheck = (index_id) => {
+  return { type: UPDATE_check, index_id };
+};
+
+export default function reducer(state = initialState, action = {}) {
+  switch (action.type) {
+    // do reducer stuff
+    // case LOAD:
+    //   //기본 데이터와 동일하게 하기 위해 앞에 안붙임
+    //   return action.quiz_list;
+    case CREATE:
+      const newMemo = [...state, action.memo];
+      return newMemo;
+    case UPDATE_check:
+      console.log(action);
+    default:
+      return state;
+  }
+}
