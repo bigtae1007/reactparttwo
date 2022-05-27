83c20ae13390f31fed5eb4cfc32fb16e516120f5
taehyeong park <b_tae96@naver.com>
2022-05-27 14:40:08 +0900

    [update] 기본 세팅

---
+++ src/redux/configStore.js
@@ -0,0 +1,13 @@
+//configStore.js
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import quiz from './modules/quiz'
+// root 리듀서를 만들어줍니다.
+// 나중에 리듀서를 여러개 만들게 되면 여기에 하나씩 추가해주는 거예요!
+const middlewares = [thunk];
+const rootReducer = combineReducers({ quiz });
+// const enhancer = applyMiddleware(...middlewares);
+// 스토어를 만듭니다.
+const store = createStore(rootReducer); //, enhancer << store
+
+export default store;
